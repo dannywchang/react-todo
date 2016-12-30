d91db311fe1ed081009b6b6716b3fd6dc27da8cb
dannywchang <dannywchang@gmail.com>
2016-12-30 12:42:34 +0800

    add test for todosearch

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -5,7 +5,7 @@ var TodoSearch = React.createClass ({
     var showCompleted = this.refs.showCompleted.checked;
     var searchText = this.refs.searchText.value;
 
-    this.props.onSerch(showCompleted, searchText);
+    this.props.onSearch(showCompleted, searchText);
   },
   render : function () {
     return (
@@ -15,7 +15,7 @@ var TodoSearch = React.createClass ({
         </div>
         <div>
           <label>
-            <input type="checkbox" ref="showCompleted" onChenge={this.handleSearch}/>
+            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
             Show completed todos
           </label>
         </div>
